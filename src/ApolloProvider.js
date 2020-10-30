3973c402b3d62ffbf6c9f1b7cdd3ac8164a24b33
Pawel <regiec.pawel@gmail.com>
2020-10-30 20:49:57 +0100

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@ap
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://warm-cove-91073.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
@@ -22,9 +22,8 @@ const client = new ApolloClient({
 })
 
 
-
 export default (
   <ApolloProvider client={client}>
-    <App/>
+    <App />
   </ApolloProvider>
-)
+);
