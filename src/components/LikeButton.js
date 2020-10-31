1e99da8558eb6c293229d789c83b684b5288e3d0
Pawel <regiec.pawel@gmail.com>
2020-10-31 11:32:49 +0100

    fixed files

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -30,7 +30,7 @@ function LikeButton({ user, post: { id, likeCount, likes } }) {
         </Button>
       )
   ) : (
-      <Button as={Link} to='/login' color='teal' basic>
+      <Button as={Link} to='/social-app/login' color='teal' basic>
         <Icon name='heart' />
       </Button>
     )
